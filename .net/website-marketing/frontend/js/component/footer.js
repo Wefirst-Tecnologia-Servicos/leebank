7083d5f17202bf08aca7028b7ef91930daeb532a
Daniel Vicente <daniel.avicente98@gmail.com>
2022-07-06 13:46:17 -0300

    Direcionamento sobre nós rodapé

--- .net/website-marketing/frontend/js/component/footer.js
+++ .net/website-marketing/frontend/js/component/footer.js
@@ -9,7 +9,10 @@ angular
     .component("footerComponent", {
         templateUrl: "js/component/template/footer.html",
         controller: function (DictionaryService, GlobalService) {
+
             var ctrl = this;
+            this.currentLanguage = GlobalService.getQueryStringParam("lang") ?? "PT";
+
             DictionaryService.getDictionary(GlobalService.getQueryStringParam("lang") ?? "PT").then(function(translations) {
                 ctrl.dictionary = translations.data;
             });
