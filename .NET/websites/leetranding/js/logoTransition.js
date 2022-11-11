8dc0afec5fe64fd5f3f5c4e1779b66f4b7c30d16
Daniel Vicente <79551606+kakuzuID@users.noreply.github.com>
2022-11-11 16:45:11 -0300

    alteração da animação do logo

--- .NET/websites/leetranding/js/logoTransition.js
+++ .NET/websites/leetranding/js/logoTransition.js
@@ -2,7 +2,7 @@ var img = document.getElementsByClassName("banner__logo");
 let scrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-    if(scrollY > img[0].y) {
+    if(scrollY > (img[0].y/4)) {
         img[0].classList.add("banner__logo--transition")
     } else {
         img[0].classList.remove("banner__logo--transition")
