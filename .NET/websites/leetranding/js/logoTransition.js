26fcb957f4dca153fe227e9e90d5d3c71bb4fad9
Daniel Vicente <79551606+kakuzuID@users.noreply.github.com>
2022-11-25 10:15:17 -0300

    atualização nas animações

--- .NET/websites/leetranding/js/logoTransition.js
+++ .NET/websites/leetranding/js/logoTransition.js
@@ -1,10 +1,20 @@
 var img = document.getElementsByClassName("banner__logo");
+var logo = document.getElementById("logo-nav");
 let scrollY = window.scrollY;
+let pass = false;
 
 window.addEventListener("scroll", () => {
-    if(scrollY > (img[0].y/4)) {
-        img[0].classList.add("banner__logo--transition")
-    } else {
-        img[0].classList.remove("banner__logo--transition")
+    if(scrollY > (img[0].y - 60) && pass == false) {
+        pass = true;
+        img[0].classList.remove("banner__logo--transitionGrow");
+        logo.classList.remove("header__logo--transitionDesappears");
+        img[0].classList.add("banner__logo--transitionShrink");
+        logo.classList.add("header__logo--transitionAppears");
+    } else if (scrollY < (img[0].y - 60) && pass == true) {
+        pass = false;
+        img[0].classList.remove("banner__logo--transitionShrink");
+        logo.classList.remove("header__logo--transitionAppears");
+        img[0].classList.add("banner__logo--transitionGrow");
+        logo.classList.add("header__logo--transitionDesappears");
     }
 });
