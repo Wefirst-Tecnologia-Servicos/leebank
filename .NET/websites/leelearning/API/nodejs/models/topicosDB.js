3f7ef99b1da20883797a0f770783446ac4c3c61a
Lucca Duarte Batista <luccaduartebatista@outlook.com>
2022-12-12 17:07:26 -0300

    API Going

---
+++ .NET/websites/leelearning/API/nodejs/models/topicosDB.js
@@ -0,0 +1,33 @@
+//Permite a abertura da conexão apenas quando necessário
+function Topico(conexao){
+    this._con = conexao;
+}
+
+//Seleciona todos os Topicos
+Topico.prototype.getTopicos = function(callback){
+    this._con.query('select * from topic order by articleid asc',callback);
+}
+
+//Realiza o insert
+Topico.prototype.insertTopico = function(Topico, callback){
+    this._con.query("insert into topic set ?",Topico,callback);
+}
+
+
+//Realiza o update
+Topico.prototype.updateTopico = function(Topico,callback){
+    this._con.query("update topic set ?  where topic = '" + Topico.topic + "'", Topico.dados,callback);
+}
+
+
+//Realiza o delete
+Topico.prototype.deleteTopico = function(Topico,callback){
+    this._con.query("delete from topic where topic = '" + Topico + "'",callback);
+}
+
+
+//Exporta a classe Equipamneto
+module.exports = ()=>{
+    return Topico;
+}
+
