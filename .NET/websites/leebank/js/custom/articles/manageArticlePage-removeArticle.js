e1545e06375d0bf9fd4de1c9d8ad8fcf764eedb9
Lucca810 <daniel.avicente98@gmail.com>
2023-01-20 14:25:43 -0300

    delete going

--- .NET/websites/leebank/js/custom/articles/manageArticlePage-removeArticle.js
+++ .NET/websites/leebank/js/custom/articles/manageArticlePage-removeArticle.js
@@ -1,5 +1,18 @@
 
 function deletarArtigo(articleid){
-   
+    var dados = {articleid: articleid}
+    var settings = {
+        "url": "https://61i4yuf829.execute-api.us-east-1.amazonaws.com/delete/artigo",
+        "method": "DELETE",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify(dados)
+    };
     
+    $.ajax(settings).done(function (data) {
+        alert("artigo removido!")
+        location.reload()
+    })
 }
